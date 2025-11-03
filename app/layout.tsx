aea254ba123d491badba3bcaa35f32eb6782ebc5
TappaKamal <kamalhussaintappa09@gmail.com>
2025-11-03 08:18:35 +0530

    logo added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,27 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ 
-  weight: ['300', '400', '500', '600', '700', '900'],
-  subsets: ['latin'],
-  display: 'swap',
-})
+const inter = Inter({
+  weight: ["300", "400", "500", "600", "700", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: 'KamalHussain Portfolio | Next.js Developer',
-  description: 'Portfolio of KamalHussain, a Next.js Developer from KL University. Creating smooth and intuitive user interfaces.',
-}
+  title: "KamalHussain Portfolio | Next.js Developer",
+  description:
+    "Portfolio of KamalHussain, a Next.js Developer from KL University. Creating smooth and intuitive user interfaces.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
-
